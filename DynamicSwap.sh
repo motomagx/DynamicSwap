23c339271dc40c10a7f586c04ddb24152c04afd9
motomagx <40811525+motomagx@users.noreply.github.com>
2021-01-12 17:07:11 -0300

    Update DynamicSwap.sh

--- DynamicSwap.sh
+++ DynamicSwap.sh
@@ -7,7 +7,7 @@
 # By default, the script will trigger an additional 256MB SWAP block, if the total free memory (swap + RAM) is less than 512MB.
 # The script will allocate blocks of 256MB in a row, and the blocks will be deactivated if there is 768MB (512 + 256MB) of RAM free, automatically.
 # When deactivating the adjacent blocks, the data will be reloaded to RAM, eliminating subsequent lags during the loading of the data that would be in the disk swap.
-# The script can also be used in conjunction with zram, which can dramatically reduce consumption and RAM in many cases.
+# The script can also be used in conjunction with zram, which can dramatically reduce consumption of RAM in many cases.
 # Note: do not use this script on a BTRFS file system, as this FS does not support swapping.
 
 LOG_DIR="/var/log/dynamicswap"
